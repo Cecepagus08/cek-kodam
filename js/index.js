bb2a8625cb374328e78b97e8168379fe28163426
Cecepagus06 <cecepagus406@gmail.com>
2024-06-21 08:15:28 +0700

    edit dikit

--- js/index.js
+++ js/index.js
@@ -26,81 +26,11 @@
     "Burung Merpati Matematika",
     "Katak Penyair",
     "Harimau Sumatera",
-  
-  
-    "Kelelawar DJ",
-    "Kelinci Astronaut",
-    "Sapi Detektif",
-    "Anjing Polisi",
-    "Bebek Insinyur",
-    "Kambing Kungfu",
-    
-    "Zebra Pelukis",
-    "Belut Elektrik",
-    "Ular Hipnotis",
-    "Burung Hantu Filosof",
-    "Badak Petinju",
-    "Flamingo Penari Balet",
-    "Kura-kura Samurai",
-    "Babi Penjaga Gawang",
-    "Domba Hacker",
-    "Angsa Pustakawan",
-
-    "Panda Chef",
-    "Elang Sniper",
-    "Tikus Ilmuwan",
-    "Ikan Paus Detektif",
-    "Serigala Matematikawan",
-    "Landak Arsitek",
-    "Burung Unta Sprinter",
-    "Hiu Penyelam",
-    "Kuda Api",
-    "Kukang Penjaga",
-    "Bunglon Aktor",
-
-    "Capung Teknisi",
-  
-    "Cendrawasih Desainer",
-    "Kakatua Motivator",
-    "Kakaktua Guru",
-    "Walrus Petani",
-    "Penguin Fotografer",
-
-    "Cheetah Pelari",
-
-    "Lebah Astronaut",
-  
-    
-    "Kuda Penari",
-    "Merpati Postman",
-    "Monyet Pianis",
-    "Kancil Sprinter",
-    "Harimau Pencari Jejak",
-    "Gajah Penyelidik",
-    "Anoa Samurai",
-    "Domba Berbakat",
-    "Buaya Penyelam",
-    "Kucing Pesulap",
-    "Cacing Hacker",
-    "Kelelawar Vokalis",
-    
-  
     "Tarantula Blogger",
     "Katak DJ",
     "Landak Dokter",
     "Sapi Balerina",
     "Kalajengking Bartender",
-
-    
-    "Paus Blogger",
-  
-    "Monyet Detektif",
-    "Lumba-lumba Ilmuwan",
-    
-    "Ular Programmer",
-    
-    "Kucing Pesilat",
-    "Ikan Cupang Samurai",
     "Srigala Hacker",
     "ubur ubur sunda",
     "kobo kkb",
@@ -108,16 +38,16 @@
     'Ayam Berkutut', 'Lumba-Lumba Demak', 'Bohlam Lampu', 'Buaya Nigeria', 'Naga Bearbend', 'Pohon Palem','Penyu Isriwil', 'Perkakas Kopling', 'Landak Trenggalek', 'Ultraman Orb'
 ];
             const randomIndex = Math.floor(Math.random() * khodams.length);
-            const khodamTerdeteksi = khodams[randomIndex];
+            const namaKodam = khodams[randomIndex];
            ///text kodam
             const pesan = [
-                `Ya, ${name}, di dalam dirimu ada khodam ${khodamTerdeteksi}.`,
+                `${name}, di dalam dirimu ada khodam ${namaKodam}.`,
                 
-                `Khodam ${khodamTerdeteksi} sedang bersemayam di dalam dirimu, ${name}.`,
+                `Khodam ${namaKodam} sedang bersemayam di dalam dirimu, ${name}.`,
                 `Kamu bersih dari khodam, ${name}.`,
-                `Khodam ${khodamTerdeteksi} berada di dalam dirimu, ${name}, jaga baik-baik!`,
+                `Khodam ${namaKodam} berada di dalam dirimu ${name}, jaga baik-baik!`,
                 `Tidak dapat mendeteksi khodam di dalam dirimu, ${name}. Coba lagi.`,
-                `terdapat kodam ${khodamTerdeteksi}, yang sedang tidur dalam dirimu ${name}`
+                `terdapat kodam ${namaKodam}, yang sedang tidur dalam dirimu ${name}`
             ];
             const randomPesanIndex = Math.floor(Math.random() * pesan.length);
             const hasilTeks = pesan[randomPesanIndex];
