c185e23be8617e697dfca2db3ea4816a0c9aad8d
Cecepagus06 <cecepagus406@gmail.com>
2024-06-22 17:12:39 +0700

    edit dikit

--- js/index.js
+++ js/index.js
@@ -13,29 +13,16 @@
     "Kepiting Merah Alaska",
     "Laba-laba Wonogiri",
     "Kudanil Drible",
-    "Bekicot Ninja",
-    "Jerapah Akrobatik",
-    "Singa Jogja",
-    "Buaya Pengacara",
     "perkedel jagung",
     "bakwan jagung",
     "martabak manis",
     "tempe raksasa",
-    "Iguana Rocker",
-    "Kucing Samurai",
-    "Burung Merpati Matematika",
-    "Katak Penyair",
-    "Harimau Sumatera",
-    "Tarantula Blogger",
-    "Katak DJ",
-    "Landak Dokter",
-    "Sapi Balerina",
-    "Kalajengking Bartender",
-    "Srigala Hacker",
-    "ubur ubur sunda",
-    "kobo kkb",
+    "sirkuit mandalika",
+    "kuntilanak merah",
+    "ubur ubur sunda", "kuyang batu bara",
+    "kobo kkb", "pisang goreng", "mangga muda", "salak", "singkong rebus",
     "Jerapah Blogger",
-    'Ayam Berkutut', 'Lumba-Lumba Demak', 'Bohlam Lampu', 'Buaya Nigeria', 'Naga Bearbend', 'Pohon Palem','Penyu Isriwil', 'Perkakas Kopling', 'Landak Trenggalek', 'Ultraman Orb'
+    'Ayam Berkutut', 'Lumba-Lumba Demak', 'Bohlam Lampu', 'Buaya Nigeria', 'Naga Bearbend', 'Pohon Palem','Penyu Isriwil', 'Perkakas Kopling', 'Landak Trenggalek', "mangga tetangga", "Rambutan", "Kucing jawa", "kakek zeus", 'Ultraman Orb'
 ];
             const randomIndex = Math.floor(Math.random() * khodams.length);
             const namaKodam = khodams[randomIndex];
@@ -51,7 +38,6 @@
             ];
             const randomPesanIndex = Math.floor(Math.random() * pesan.length);
             const hasilTeks = pesan[randomPesanIndex];
-
             document.getElementById('result').textContent = hasilTeks;
               
             }
